e02f286965b36beba1d457ceeb31505fc1e18fee
rupesh1241 <rupesh1241@gmail.com>
2020-06-07 07:49:11 +0530

    OOPS - Class and Objects

---
+++ CShaprOOPS/ClassAndObjects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShaprOOPS
+{
+    class ClassAndObjects
+    {
+        public ClassAndObjects()
+        {
+            Console.WriteLine("This is the ClassAndObjects default consturctor ");
+        }
+        public ClassAndObjects(string param)
+        {
+            Console.WriteLine(String.Format("This is the ClassAndObjects parameterized consturctor with value  {0}",param));
+        }
+
+        public void ClassAndObjectsMethod1()
+        {
+            Console.WriteLine("This is object call for ClassAndObjectsMethod");
+        }
+
+        public static void ClassAndObjectsMethod2()
+        {
+            Console.WriteLine("This is static ClassAndObjectsMethod");
+        }
+    }
+}
---
+++ CShaprOOPS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShaprOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crating Objects for class with consturctor and static methods
+            ClassAndObjects.ClassAndObjectsMethod2();
+            ClassAndObjects obj = new ClassAndObjects();
+            obj.ClassAndObjectsMethod1();
+            ClassAndObjects obj1 = new ClassAndObjects("Value1");
+            obj1.ClassAndObjectsMethod1();
+            Console.ReadLine();
+        }
+    }
+}
