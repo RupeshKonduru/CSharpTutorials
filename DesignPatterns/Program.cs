59e3f18acd18e1157d23adc2647e03c1edeba46d
rupesh1241 <rupesh1241@gmail.com>
2020-06-07 15:30:08 +0530

    Adding other projects

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programs");
+        }
+    }
+}
