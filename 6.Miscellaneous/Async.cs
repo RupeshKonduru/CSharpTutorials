3a2763103c88d9a12142938a499527652cea68be
rupesh1241 <rupesh1241@gmail.com>
2020-06-08 21:53:15 +0530

    Async Await

---
+++ 6.Miscellaneous/Async.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _6.Miscellaneous
+{
+    class Asyncncrous
+    {
+        static void Main()
+        {
+            //Task task = new Task(CallMethod);
+            //task.Start();
+            //task.Wait();
+
+
+            Method1();
+            Method2();
+            Console.ReadLine();
+        }
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                }
+            });
+        }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
+
+        static async void CallMethod()
+        {
+            string filePath = "D:\\sampleFile.txt";
+            Task<int> task = ReadFile(filePath);
+
+            Console.WriteLine(" Other Work 1");
+            Console.WriteLine(" Other Work 2");
+            Console.WriteLine(" Other Work 3");
+
+            int length = await task;
+            Console.WriteLine(" Total length: " + length);
+
+            Console.WriteLine(" After work 1");
+            Console.WriteLine(" After work 2");
+        }
+
+        static async Task<int> ReadFile(string file)
+        {
+            int length = 0;
+
+            Console.WriteLine(" File reading is stating");
+            using (StreamReader reader = new StreamReader(file))
+            {
+                // Reads all characters from the current position to the end of the stream asynchronously    
+                // and returns them as one string.    
+                string s = await reader.ReadToEndAsync();
+
+                length = s.Length;
+            }
+            Console.WriteLine(" File reading is completed");
+            return length;
+        }
+    }
+}
---
+++ 6.Miscellaneous/AyncAwait.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _6.Miscellaneous
+{
+    public class AsyncAwait
+    {
+        
+
+        void DoAsTask()
+        {
+            WriteOutput("1 - Starting");
+            var t = Task.Factory.StartNew<int>(DoSomethingThatTakesTime);
+            WriteOutput("2 - Task started");
+            t.Wait();
+            WriteOutput("3 - Task completed with result: " + t.Result);
+        }
+
+        async Task DoAsAsync()
+        {
+            WriteOutput("1 - Starting");
+            var t = Task.Factory.StartNew<int>(DoSomethingThatTakesTime);
+            WriteOutput("2 - Task started");
+            var result = await t;
+            WriteOutput("3 - Task completed with result: " + result);
+        }
+
+        int DoSomethingThatTakesTime()
+        {
+            WriteOutput("A - Started something");
+            Thread.Sleep(1000);
+            WriteOutput("B - Completed something");
+            return 123;
+        }
+
+        public void WriteOutput(string message)
+        {
+            Console.WriteLine("[{0}] {1}", Thread.CurrentThread.ManagedThreadId, message);
+        }
+        static void Main()
+        {
+            AsyncAwait asv = new AsyncAwait();
+            asv.WriteOutput("Program Begin");
+            // DoAsTask();
+            asv.DoAsAsync();
+            asv.WriteOutput("Program End");
+            Console.ReadLine();
+        }
+    }
+}
