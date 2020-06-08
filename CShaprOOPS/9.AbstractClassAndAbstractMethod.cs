4a627547680a30e9d82490732391af631e9740b9
rupesh1241 <rupesh1241@gmail.com>
2020-06-08 09:30:42 +0530

    Abstract Class and Abstract Method

--- CShaprOOPS/9.AbstractClassAndAbstractMethod.cs
+++ CShaprOOPS/9.AbstractClassAndAbstractMethod.cs
@@ -6,11 +6,45 @@ using System.Threading.Tasks;
 
 namespace CShaprOOPS
 {
-    class AbstractClassAndAbstractMethod
+    public abstract class AbstractClassAndAbstractMethod
     {
-        static void Main()
+        protected double rate;
+        public abstract void getRate();
+        public void calculation(int units)
         {
-            Console.ReadLine();           
+            Console.Write("BILL AMOUNT FOR " + units + " UNITS is: Rs.");
+            Console.WriteLine(rate * units);
         }
     }
+    class CommercialPlan : AbstractClassAndAbstractMethod
+    {
+        public override void getRate()
+        {
+            rate = 5.00;
+        }
+    }
+    class DomesticlPlan : AbstractClassAndAbstractMethod
+    {
+        public override void getRate()
+        {
+            rate = 2.50;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClassAndAbstractMethod p;
+            Console.WriteLine("COMMERCIAL CONNECTION");
+            p = new CommercialPlan();
+            p.getRate();
+            p.calculation(250);
+            Console.WriteLine("DOMESTIC CONNECTION");
+            p = new DomesticlPlan();
+            p.getRate();
+            p.calculation(150);
+            Console.ReadKey();
+        }
+    }
+   
 }
---
+++ CShaprOOPS/9.AbstractMethod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CShaprOOPS
+{
+    abstract class AbstractMethod
+    {
+        protected int EmpId, EAge;
+        protected string EName, EAddress;
+        public abstract void GetEmpData();
+        public virtual void DisplayEmpData()
+        {
+            Console.WriteLine("Employee Id Is: " + this.EmpId);
+            Console.WriteLine("Employee Name Is: " + this.EName);
+            Console.WriteLine("Employee Address Is: " + EAddress);
+            Console.WriteLine("Employee Age is: " + this.EAge);
+        }
+        public AbstractMethod()
+        {
+            Console.WriteLine("ABSTRACT CLASS CONSTRUCTOR");
+        }
+    }
+    class ClsManager : AbstractMethod
+    {
+        double Bonus, CA;
+        public override void GetEmpData()
+        {
+            Console.WriteLine("ENTER MANAGER DETAILS");
+            Console.WriteLine("Enter the Manager Id: ");
+            EmpId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Manager name: ");
+            EName = Console.ReadLine();
+            Console.WriteLine("Enter the manager Bonus: ");
+            Bonus = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Manager CA: ");
+            CA = double.Parse(Console.ReadLine());
+        }
+        public override void DisplayEmpData()
+        {
+            Console.WriteLine("manager id is: " + EmpId);
+            Console.WriteLine("manager name is: " + EName);
+            Console.WriteLine("manager bonus is: " + Bonus);
+            Console.WriteLine("Manager CA is: " + CA);
+        }
+        static void Main(string[] args)
+        {
+            ClsManager cm = new ClsManager();
+            cm.GetEmpData();
+            cm.DisplayEmpData();
+            Console.ReadKey();
+        }
+    }
+}
