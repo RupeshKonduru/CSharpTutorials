89d8f7a29946b7fdbf6a137753172dd470137ea2
rupesh1241 <rupesh1241@gmail.com>
2020-06-12 13:05:21 +0530

    Fluent Migrator

---
+++ RepositoryPatternWeb/App_Start/FluentRun.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryPatternWeb.App_Start
+{
+    public class FluentRun
+    {
+    }
+}
---
+++ RepositoryPatternWeb/FluentMigrator/FluentMigrator.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryPatternWeb.FluentMigrator
+{
+    public class FluentMigrator
+    {
+        [Migration(20180430121800)]
+        public class AddLogTable : Migration
+        {
+            public override void Up()
+            {
+                Create.Table("Log")
+                    .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                    .WithColumn("Text").AsString();
+            }
+
+            public override void Down()
+            {
+                Delete.Table("Log");
+            }
+        }
+    }
+}
