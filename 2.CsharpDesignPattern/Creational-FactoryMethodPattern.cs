662ba49efc8859e2008d436504bb8539e34d09d6
rupesh1241 <rupesh1241@gmail.com>
2020-06-13 15:47:46 +0530

    Solution Changes

--- 2.CsharpDesignPattern/Creational-FactoryMethodPattern.cs
+++ 2.CsharpDesignPattern/Creational-FactoryMethodPattern.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CsharpDesignPattern
+namespace DesignPatterns
 {
     public abstract class CreditCardFactoryMethod
     {
@@ -37,35 +37,35 @@ namespace CsharpDesignPattern
             return product;
         }
     }
-    class FactoryMethod
-    {
-        static void Main(string[] args)
-        {
+    //class FactoryMethod
+    //{
+    //    static void Main(string[] args)
+    //    {
 
-            CreditCard creditCard = new PlatinumFactory().CreateProduct();
-            if (creditCard != null)
-            {
-                Console.WriteLine("Card Type : " + creditCard.GetCardType());
-                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
-                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
-            }
-            else
-            {
-                Console.Write("Invalid Card Type");
-            }
-            Console.WriteLine("--------------");
-            creditCard = new MoneyBackFactory().CreateProduct();
-            if (creditCard != null)
-            {
-                Console.WriteLine("Card Type : " + creditCard.GetCardType());
-                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
-                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
-            }
-            else
-            {
-                Console.Write("Invalid Card Type");
-            }
-            Console.ReadLine();
-        }
-    }
+    //        CreditCard creditCard = new PlatinumFactory().CreateProduct();
+    //        if (creditCard != null)
+    //        {
+    //            Console.WriteLine("Card Type : " + creditCard.GetCardType());
+    //            Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+    //            Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+    //        }
+    //        else
+    //        {
+    //            Console.Write("Invalid Card Type");
+    //        }
+    //        Console.WriteLine("--------------");
+    //        creditCard = new MoneyBackFactory().CreateProduct();
+    //        if (creditCard != null)
+    //        {
+    //            Console.WriteLine("Card Type : " + creditCard.GetCardType());
+    //            Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+    //            Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+    //        }
+    //        else
+    //        {
+    //            Console.Write("Invalid Card Type");
+    //        }
+    //        Console.ReadLine();
+    //    }
+    //}
 }
