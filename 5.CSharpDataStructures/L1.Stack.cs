04d6a3ec10d15e47e12967fc5b61dc48b90529d2
rupesh1241 <rupesh1241@gmail.com>
2020-06-19 09:25:45 +0530

    Started DataStructure Programs

---
+++ 5.CSharpDataStructures/L1.Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLogicalPrograms
+{
+    public class SamplesStack
+    {
+        public static void Main()
+        {
+
+            // Creates and initializes a new Stack.
+            Stack myStack = new Stack();
+            myStack.Push("Hello");
+            myStack.Push("World");
+            myStack.Push("!");
+            myStack.Pop();
+            // Displays the properties and values of the Stack.
+            Console.WriteLine("myStack");
+            Console.WriteLine("\tCount:    {0}", myStack.Count);
+            Console.Write("\tValues:");
+            PrintValues(myStack);
+        }
+
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("    {0}", obj);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+
+
+    /* 
+    This code produces the following output.
+    myStack
+        Count:    3
+        Values:    !    World    Hello
+    */
+}
---
+++ 7.Miscelleneaous/Demo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscelleneaous
+{
+    class Demo
+    {
+        
+
+        //static void Main()
+        //{
+        //    Console.ReadKey();
+        //}
+    }
+    public interface IAnimal
+    {
+        string Speak();
+
+    }
+    class Cat : IAnimal
+    {
+       public  string Speak()
+        {
+            return "Meow";
+        }
+    }
+    class Lion : IAnimal
+    {
+        public string Speak()
+        {
+            return "Roar";
+        }
+    }
+    public  abstract class AnimalFactory
+    {
+        public abstract IAnimal GetAnimal(string animalType);
+
+        public static AnimalFactory GetAnimalFactory(string animalFactory)
+        {
+            if(animalFactory=="Sea")
+            {
+                return new SeaAnimalFactory();
+            }
+            else if (animalFactory == "Land")
+            {
+                return new LandAnimalFactory();
+            }
+            return null;
+        }
+
+    }
+
+    class SeaAnimalFactory : AnimalFactory
+    {
+        public override IAnimal GetAnimal(string animalType)
+        {
+            if (animalType == "Cat")
+            {
+                return new Cat();
+            }
+            else
+                return null;
+        }
+    }
+    class LandAnimalFactory : AnimalFactory
+    {
+        public override IAnimal GetAnimal(string animalType)
+        {
+            if (animalType == "Lion")
+            {
+                return new Lion();
+            }
+            else
+                return null;
+        }
+    }
+    class DemosAbstract
+    {
+        public static void Main()
+        {
+            var anmialFactory = AnimalFactory.GetAnimalFactory("sea");
+            var getANmialType = anmialFactory.GetAnimal("cat");
+            var speak = getANmialType.Speak();
+
+            var getType = getANmialType.GetType();
+        }
+    }
+}
